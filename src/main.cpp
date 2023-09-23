81a00ae6aecaab754b4bd4b6a23a2cea68cdfcab
rtk0c <mail.tianyig@gmail.com>
2023-09-22 20:27:32 -0700

    Fix incorrect file loading logic

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,11 @@ int main(int argc, char** argv) {
             return -1;
         }
 
-        ifs.seekg(0, std::ios::end);
-        size_t input_size = ifs.tellg();
+        std::stringstream buffer;
+        buffer << ifs.rdbuf();
 
-        auto buf = std::make_unique<char[]>(input_size);
-        ifs.seekg(0);
-        ifs.read(buf.get(), input_size);
-
-        std::string_view input(buf.get(), input_size);
-        
         yawarakai::Heap heap;
-        auto sexps = yawarakai::parse_sexp(input, heap);
+        auto sexps = yawarakai::parse_sexp(buffer.view(), heap);
         for (auto& sexp : sexps) {
             std::cout << yawarakai::dump_sexp(sexp, heap) << '\n';
         }
